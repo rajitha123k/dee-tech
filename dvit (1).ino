7356d87b6da10be94e90ccee51e5b61ecac62b7f
rajitha123k <rajithabhavani.kantheti@gmail.com>
2020-10-12 20:35:10 -0400

    Add files via upload

---
+++ dvit (1).ino
@@ -0,0 +1,110 @@
+#include <ESP8266WiFi.h>
+int ReadUVintensityPin = A0; //Output from the sensor
+const char* ssid     = "rajitha";
+const char* password = "helloppl";
+const char* host = "api.thingspeak.com";
+const char* privateKey = "DTTGR3F1I5QSE388";
+String charIn;
+void setup()
+{
+  pinMode(ReadUVintensityPin, INPUT);
+  Serial.begin(115200); //open serial port, set the baud rate to 9600 bps
+  Serial.println("Starting up...");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop()
+{
+  int uvLevel = averageAnalogRead(ReadUVintensityPin);
+
+  float outputVoltage = 5.0 * uvLevel/1024;
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
+
+  Serial.print("UVAnalogOutput: ");
+  Serial.print(uvLevel);
+
+  Serial.print(" OutputVoltage: ");
+  Serial.print(outputVoltage);
+
+  Serial.print(" UV Intensity: ");
+  Serial.print(uvIntensity);
+  Serial.print(" mW/cm^2");
+
+  Serial.println(); 
+  delay(150);
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/update";
+  url += "?api_key=";
+  url += privateKey;
+  url += "&field1=";
+  url += uvIntensity;
+
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  while(client.connected() && !client.available()) delay(1); //waits for data
+  while (client.connected() || client.available())
+  {
+  charIn = client.readString();
+  }
+  Serial.print(charIn);
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+  
+}
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
